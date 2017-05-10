bb230f59e0b72985410fbf75663fbac3b776715a
sontt9 <son.thai@live.com>
2017-05-10 09:28:14 +0700

    Init Elixir Lib

---
+++ lib/logger_pushover_backend.ex
@@ -0,0 +1,18 @@
+defmodule LoggerPushoverBackend do
+  @moduledoc """
+  Documentation for LoggerPushoverBackend.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoggerPushoverBackend.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/logger_pushover_backend_test.exs
@@ -0,0 +1,8 @@
+defmodule LoggerPushoverBackendTest do
+  use ExUnit.Case
+  doctest LoggerPushoverBackend
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
